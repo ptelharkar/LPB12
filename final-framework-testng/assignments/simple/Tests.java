da7ca828112295271d978321194f53729330f308
PRAKASHTELHARKAR <PRAKASHTELHARKAR@DESKTOP-5IAPKIA.in.ibm.com>
2019-11-18 10:46:17 +0530

    Updated Assignments.

---
+++ final-framework-testng/assignments/simple/Tests.java
@@ -0,0 +1,24 @@
+package simple;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Tests 
+{
+	@Test
+	public void magentoLogin()
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://magento.com");
+		driver.findElement(By.className("fa-user")).click();
+		driver.quit();
+	}
+	
+
+}
